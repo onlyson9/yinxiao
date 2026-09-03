d2a008f7573c0f37cefcf0f51ede52c2fdb2642a
onlyson9 <76731660+onlyson9@users.noreply.github.com>
2026-09-04 01:00:11 +0900

    Delete 1 directory

--- 1/loader.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    var s = document.createElement('script');
-    s.src = chrome.runtime.getURL('inject.js');
-    s.onload = function () { this.remove(); };
-    (document.head || document.documentElement).appendChild(s);
-})();
