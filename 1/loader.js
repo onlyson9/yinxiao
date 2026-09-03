107ab4e46a03ac78423c4cf3c8e61dcdf8b6b436
onlyson9 <76731660+onlyson9@users.noreply.github.com>
2026-09-04 00:58:48 +0900

    Add files via upload

---
+++ 1/loader.js
@@ -0,0 +1,6 @@
+(function () {
+    var s = document.createElement('script');
+    s.src = chrome.runtime.getURL('inject.js');
+    s.onload = function () { this.remove(); };
+    (document.head || document.documentElement).appendChild(s);
+})();
